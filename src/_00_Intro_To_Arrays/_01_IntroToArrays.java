4767c41dddb025bbb177661c1bf06022623d4572
league <ashkonsaeidi@gmail.copm>
2023-06-05 18:29:00 -0700

    Make ints and everytime they reach y500 it increases by 1

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -5,32 +5,57 @@ import java.util.Random;
 public class _01_IntroToArrays {
     public static void main(String[] args) {
         // 1. declare and Initialize an array 5 Strings
-
+String[] strng = new String[5];
+strng[0]="2";
+strng[1]="3";
+strng[2]="4";
+strng[3]="5";
+strng[4]="6";
         // 2. print the third element in the array
-
+System.out.println(strng[2]);
         // 3. set the third element to a different value
-
+strng[2]="a different value";
         // 4. print the third element again
-
+System.out.println(strng[2]);
         // 5. use a for loop to set all the elements in the array to a string
         //    of your choice
-
+for (int i = 0; i < strng.length; i++) {
+	strng[i]="I couldn't think of anything funny";
+}
         // 6. use a for loop to print all the values in the array
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
-
+for (int i = 0; i < strng.length; i++) {
+	System.out.println(strng[i]);
+}
         // 7. make an array of 50 integers
+Integer[] ger = new Integer[50];
 
         // 8. use a for loop to make every value of the integer array a random
         //    number
-
+Random r = new Random();
+for (int i = 0; i < ger.length; i++) {
+	ger[i]=r.nextInt(50)+50;
+}
         // 9. without printing the entire array, print only the smallest number
         //    on the array
-
+int smallint=150;
+for (int i = 0; i < ger.length; i++) {
+	System.out.println(ger[i]);
+	if(smallint>ger[i]) {
+		smallint=ger[i];
+	}
+}
+System.out.println("\n" + smallint);
         // 10 print the entire array to see if step 8 was correct
-
+for (int i = 0; i < ger.length; i++) {
+	
+	if(smallint<ger[i]) {
+		smallint=ger[i];
+	}
+}
         // 11. print the largest number in the array.
-
+System.out.println("\n" + smallint);
         // 12. print only the last element in the array
-
+System.out.println(ger[49]);
     }
 }
--- src/_00_Intro_To_Arrays/_02_RobotRace.java
+++ src/_00_Intro_To_Arrays/_02_RobotRace.java
@@ -3,16 +3,52 @@ package _00_Intro_To_Arrays;
 import java.util.Random;
 
 import javax.swing.JOptionPane;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
 
 import org.jointheleague.graphical.robot.Robot;
 
 public class _02_RobotRace {
     // 1. make a main method
-
+public static void main(String[] args) {
+	boolean hadwon=false;
+Random r = new Random();
         // 2. create an array of 5 robots.
-
+Robot[] rob = new Robot[5];
+Integer[] ger = new Integer[5];
+for (int i = 0; i < ger.length; i++) {
+	ger[i]=0;
+}
         // 3. use a for loop to initialize the robots.
+for(int i=0; i<rob.length; i++) {
+	rob[i]= new Robot();
+	rob[i].setSpeed(5);
+	rob[i].moveTo(50+100*i, 500);
+	
+	
+}
+while(!hadwon){
+	for(int i=0; i<rob.length; i++) {
+	ger[i]+=r.nextInt(50);
+	rob[i].move(ger[i]);
+	rob[i].setAngle(ger[i]);
+	
+	
+	
+	
+	if(rob[i].getY()<=0) {
+		hadwon=true;
+		JOptionPane.showMessageDialog(null, "Robot " + (i+1) + " has one!");
+		break;
+	}
+}
+}
 
+while(hadwon) {
+	for(int i=0; i<rob.length; i++) {
+	rob[i].moveTo(r.nextInt(500)+50, r.nextInt(500)+50);
+	
+	}
+	}
         // 4. make each robot start at the bottom of the screen, side by side, facing up
     
         // 5. use another for loop to iterate through the array and make each robot move
@@ -26,5 +62,5 @@ public class _02_RobotRace {
         // 8. try different races with different amounts of robots.
     
         // 9. make the robots race around a circular track.
-
+}
 }
